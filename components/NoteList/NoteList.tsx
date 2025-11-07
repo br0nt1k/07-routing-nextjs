28094feaaddcf7708ce76a0c7b70041d577ffb0a
Oleh Oprysk <opryskoleh2006@gmail.com>
2025-11-07 22:41:28 +0200

    fixed problems

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -12,7 +12,7 @@ export default function NoteList({ notes }: NoteListProps) {
   const queryClient = useQueryClient();
 
   const deleteNoteMutation = useMutation({
-    mutationFn: (noteId: number) => deleteNote(noteId),
+    mutationFn: (id: string) => deleteNote(id),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["notes"] });
     },
